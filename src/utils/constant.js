ec3684df225688fb8d1a431709018aeb92499e56
maleek-s <malik1@windowslive.com>
2025-03-02 17:08:33 +0100

    updated endpoint URLs

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,3 @@
-export const USER_API_END_POINT="https://jobsintech.live/api/v1/user";
-export const JOB_API_END_POINT="https://jobsintech.live/api/v1/job";
-export const APPLICATION_API_END_POINT="https://jobsintech.live/api/v1/save"; 
+export const USER_API_END_POINT="https://job-portal-r.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-r.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-portal-r.onrender.com/api/v1/save"; 
